43cc3c98a45dc047c8733506be5e0488d6923f4d
Alexander Smith <l2blackbelt@users.noreply.github.com>
2019-02-10 14:56:16 -0600

    added leadboard (very important)
    I think this is as far as I can go in making this attractive to new people

--- README.md
+++ README.md
@@ -1,30 +1,32 @@
 # GameOfPureStrategy
-python classes to play GOPS
+
+Battlin' strategies to win the most games of the [Game of Pure Strategy (GOPS)](https://en.wikipedia.org/wiki/Goofspiel)
+
+
+### [Check out the current bot leaderboard here!](bot_scores.md)
 
 ## What in the sweet Jesus is this?
 I thought the Game Of Pure Stratergy was really cool, so I created a python class, GameOfPureStrategy, to simulate a game of it.
-Then I created GameArena so I could simulate multiple games in a row.  Then I started creating more complicated bots.  I'm not sure what happened, but I'm here, with a very flexible and friendly implementation of GOPS in python, where competing "solutions" can fight head to head for the most wins in nerdy combat.
+Then I created GameArena so I could simulate multiple games in a row.  Then I started creating ever more complicated bots to play.  I'm not sure what happened, now I have a very flexible implementation of GOPS in python, **where competing "solutions" can fight head to head for the most wins in nerdy combat.**
 
 And now that you're here, we're here together.
 
 If you dig this half as much as I do, perhaps you want to make a bot and see how it performs.  If so, here's what you need to know:
 
 ## How do I use this?
+* play_GOPS.py is the module that kicks off games.
+	* Bots are passed by the class to GameArena, so you can easily modify:
+		* which bots participate, 
+		* how many games to play
+		* how to print to console.  (useful for figuring out how bots play and tweaking strategy)
 
-play_GOPS.py is the module that kicks off games.  
-
-You can easily modify which bots participate, how many games to play, and how to print to console.
-
-If you want to make a new bot, check out bots/A_samplebot.py which provides a template, along with instructions.
-
-
-## Current Champs to beat
+## Think you can do better?
 I do my official tests with 2 bots, 100,000 runs, and 13 cards.
 
+1. Create your bot(s), and place them in a new python file in the [bots](bots) directory. 
+	* *Check out required bot format in [bots/A_sampleBot.py](bots/A_sampleBot.py)*
+2. Send me a pull request with your bot.  I'll add your bot to the leaderboard :)
 
-Right now, the (semi) deterministic bot to beat is InterestingBot, which handly beats other deterministic bots, and creams RandomBot up to 86% of the time.
-
-The Learning bot to beat is LearningBot, which absolutely curb-stomps InterestingBot 80% of the time.
 
 
-If you think your bot is the shiznit, send me a pull request, and we shall do battle.
+
--- scoreboard.py
+++ scoreboard.py
@@ -1,74 +1,129 @@
 #!/usr/bin/python
 
 from utils.log import log
+import json
 
-from bots import *
 #make python 2 and 3 behave the same for raw input
 if hasattr(__builtins__, 'raw_input'):
     input = raw_input
 
 
 
+data_file = "data.json"
 
-
-
-
-#import all the bots
-from bots.simpleBots import BasicBot, ObviousBot, RandomBot, HumanBot, ObviousPlusOneBot
-from bots.alexBots import LearningBot, WatchingBot, InterestingBot
-from bots.philBots import PhillipBotUpBot
-from bots.brianBots import GreedBot, SafeBetBot, OddBot
-
-
-
-#any bot class names to leave off the scoreboard for various reasons.
-bots_to_skip = ["HumanBot"]
-
-
-
-
-import inspect, sys, itertools, json, time
-
-#getattr(bots, "HumanBot")
-
-clsmembers = inspect.getmembers(sys.modules[__name__], inspect.isclass)
-print(clsmembers)
-
-
-
-#########rest of imports go here ###########
-from gameArena import GameArena
-############################################
 
 
 num_games = 100000
 num_cards = 13
 
+#any bot class names to leave off the scoreboard for various reasons.
+bots_to_skip = ["HumanBot", "WatchingBot"]
 
-start = time.time()
+def generate_json():
+	#import all the bots
+	from bots.simpleBots import BasicBot, ObviousBot, RandomBot, HumanBot, ObviousPlusOneBot
+	from bots.alexBots import LearningBot, WatchingBot, InterestingBot
+	from bots.philBots import PhillipBotUpBot
+	from bots.brianBots import GreedBot, SafeBetBot, OddBot
 
-bot_names = [bot[0] for bot in clsmembers]
-bot_results = { name:{} for name in bot_names}
-
-
-for combination in itertools.combinations(clsmembers,2):
-	bot1_name, bot1_class = combination[0]
-	bot2_name, bot2_class = combination[1]
-
-	if bot1_name in bots_to_skip or bot2_name in bots_to_skip:
-		continue
-
-	print(str(bot1_name)+" vs "+str(bot2_name))
-	game = GameArena(num_cards=num_cards, num_games=num_games, player_arr=[bot1_class, bot2_class])
-	bot1_score, bot2_score = game.play(play_method = "quiet")
+	import inspect, sys, itertools, time
 	
-	#update player results
-	bot_results[bot1_name][bot2_name] = bot1_score
-	bot_results[bot2_name][bot1_name] = bot2_score
+	
+	clsmembers = inspect.getmembers(sys.modules[__name__], inspect.isclass)
+	print(clsmembers)
+	
+	
+	#########rest of imports go here ###########
+	from gameArena import GameArena
+	############################################
+	
+	start = time.time()
+	
+	bot_names = [bot[0] for bot in clsmembers]
+	bot_results = { name:{} for name in bot_names}
+	
+	
+	for combination in itertools.combinations(clsmembers,2):
+		bot1_name, bot1_class = combination[0]
+		bot2_name, bot2_class = combination[1]
+	
+		if bot1_name in bots_to_skip or bot2_name in bots_to_skip:
+			continue
+	
+		print(str(bot1_name)+" vs "+str(bot2_name))
+		game = GameArena(num_cards=num_cards, num_games=num_games, player_arr=[bot1_class, bot2_class])
+		bot1_score, bot2_score = game.play(play_method = "quiet")
+		
+		#update player results
+		bot_results[bot1_name][bot2_name] = bot1_score
+		bot_results[bot2_name][bot1_name] = bot2_score
+	
+	
+	with open(data_file, 'w') as outfile:
+	    json.dump(bot_results, outfile, indent=4, sort_keys=True)
+	print(bot_results)
+	
+	print("Completed scoreboard in "+str(time.time()-start)+" seconds")
 
 
-with open('data.json', 'w') as outfile:
-    json.dump(bot_results, outfile, indent=4, sort_keys=True)
-print(bot_results)
 
-print("Completed scoreboard in "+str(time.time()-start)+" seconds")
+
+def generate_scoreboard():
+	import operator
+	with open(data_file) as f:
+		data = json.load(f)
+
+		#get wins per bot
+		wins_per_bot = { name:{} for name in data.keys() if name not in bots_to_skip}
+		for bot_name, result in data.items():
+			if bot_name in bots_to_skip:
+				continue
+			wins = 0
+			for result_name, result_number in result.items():
+				if result_name in bots_to_skip:
+					continue
+				if result_number > num_games/2:
+					wins+=1
+			wins_per_bot[bot_name] = wins
+
+		sorted_bots = sorted(wins_per_bot.items(), key=operator.itemgetter(1))
+		sorted_bots.reverse()
+		#print(sorted_bots)
+		
+
+		#print ranked bots
+		with open("bot_scores.md", "w") as o:
+			o.write("# Bot Scores\n")
+			o.write("Results of "+str(num_games)+" 1v1 games, with "+str(num_cards)+" cards each\n\n")
+			o.write("|rank|bot|score|results|\n")
+			o.write("|-----|-----|-----|-----|\n")
+			i=0
+			last_wins = num_games
+			for bot_name, wins in sorted_bots:
+				if wins < last_wins:
+					i+=1 #handle ties
+				last_wins = wins
+				#be obnoxious about scores
+
+
+				o.write("|#"+str(i)+"|"+str(bot_name)+"|"+str(wins)+"|")
+
+				sorted_opponents = sorted(data[bot_name].items(), key=operator.itemgetter(1))
+				sorted_opponents.reverse()
+				for opponent,won_games in sorted_opponents:
+					if opponent in bots_to_skip:
+						continue
+					win_percent = round(won_games/num_games*100,2)
+					if win_percent > 50:
+						o.write("WIN ")
+					else:
+						o.write("LOSS ")
+					o.write(str(win_percent)+"% vs "+str(opponent)+"<br>")
+				o.write("|\n")
+
+
+
+if __name__== "__main__":
+	#generate_json()
+	generate_scoreboard()
+
