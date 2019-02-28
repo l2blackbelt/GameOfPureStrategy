21d656f2c7a630691ce7d227d5996f955d2e294a
Alexander Smith <l2blackbelt@users.noreply.github.com>
2019-02-27 22:32:52 -0600

    scoreboard auto-imports new python modules

--- bots/__init__.py
+++
@@ -1,6 +0,0 @@
-#import all the bots so they're accessable for inspection at the top level "bots" package
-from .simpleBots import *
-from .alexBots import *
-from .brianBots import *
-from .philBots import *
-from .JamesBots import *
