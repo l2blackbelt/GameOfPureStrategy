277428248b77f12e99fcf837cdf1038395873826
Alex <arosmit3@gmail.com>
2019-03-01 19:22:51 -0600

    update priot to magic autocommit
    I needed to purge the git history with fire after autocommit accidentlly fucked up the entire history.  Will try to be more responsible in the future.

--- Dockerfile
+++ Dockerfile
@@ -9,5 +9,18 @@ WORKDIR /app
 # Copy the current directory contents into the container at /app
 COPY . /app
 
+ADD /.ssh/id_rsa .
+ADD /.ssh/known_hosts .
+
+ENV GIT_REPO="git@github.com:l2blackbelt/GameOfPureStrategy.git"
+ENV GIT_BRANCH="master"
+ENV GIT_ORIGIN="origin"
+ENV COMMIT_USER="Magic Auto-build"
+ENV COMMIT_EMAIL="<>"
+ENV WORKING_DIR="."
+ENV FILES_TO_COMMIT="."
+
+
+
 # Run app.py when the container launches
-CMD ["python3", "regression.py"]
+CMD python3 scoreboard.py; /bin/sh entrypoint.sh
--- entrypoint.sh
+++ entrypoint.sh
@@ -8,54 +8,45 @@ set -e -x
 [ -z "${COMMIT_USER}" ] && { echo "Need to set COMMIT_USER"; exit 1; }
 [ -z "${COMMIT_EMAIL}" ] && { echo "Need to set COMMIT_EMAIL"; exit 1; }
 [ -z "${WORKING_DIR}" ] && { echo "Need to set WORKING_DIR"; exit 1; }
-[ -z "${SSH_KEY}" ] && { echo "Need to set SSH_KEY"; exit 1; }
 [ -z "${FILES_TO_COMMIT}" ] && { echo "Need to set FILES_TO_COMMIT"; exit 1; }
 
 # Change to our working directory
 cd ${WORKING_DIR}
 
+
 # Set up our SSH Key
 if [ ! -d ~/.ssh ]; then
 	echo "SSH Key was not found. Configuring SSH Key."
 	mkdir ~/.ssh
-	cp "${SSH_KEY}" ~/.ssh/id_rsa
+	cp known_hosts ~/.ssh
+	cp id_rsa ~/.ssh
 	chmod 700 ~/.ssh
 	chmod 600 ~/.ssh/id_rsa
 
-	echo -e "Host *\n    StrictHostKeyChecking no\n    UserKnownHostsFile=/dev/null\n" > ~/.ssh/config
-fi
-
-# Check to see if the given directory already has an initialized
-# git repository.
-if [ ! -d "${WORKING_DIR}/.git" ]; then
-	echo "Git repository not found. Initializing repository."
-	git init 
-	git remote add ${GIT_ORIGIN} ${GIT_REPO}
-	git fetch
-	git checkout -t ${GIT_ORIGIN}/${GIT_BRANCH}
+	#echo -e "Host *\n    StrictHostKeyChecking=no\n    UserKnownHostsFile=/dev/null\n" > ~/.ssh/config
 fi
 
 # Configure our user and email to commit as.
 git config --global user.name "${COMMIT_USER}"
 git config --global user.email "${COMMIT_EMAIL}"
 
+#change repo from https to ssh
+git remote set-url origin "${GIT_REPO}"
 
 
-# Check to see if there are changes
-CHANGES=`git status -s | awk {'print $2'}`
-if [ -z "${CHANGES}" ]; then
-	echo "No changes detected."
-	continue
-fi
-
-# Check to see if we need to commit all.
-git add "data.json" "bot_scores.md" 
-CHANGES_FOUND="1"
 git status
 
+git stash
+git checkout ${GIT_BRANCH}
+git pull
+
+#overwrite instead of merge
+git checkout stash -- .
+
+git add "data.json" "bot_scores.md"
+
+
 # Commit and push the detected changes if they are found.
-if [ ! -z "${CHANGES_FOUND}" ]; then
-	echo "Changes detected."
-	git commit -m "Auto Build Scoreboard."
-	git push ${GIT_ORIGIN} ${GIT_BRANCH}
-fi
+git commit -m "Auto Build Scoreboard."
+git push ${GIT_ORIGIN} ${GIT_BRANCH}
+
